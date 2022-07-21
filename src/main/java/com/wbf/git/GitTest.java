75dcbfd4a0169e8452a3c459608eb8b9eae2bf47
Wbf <3496628170@qq.com>
2022-07-21 17:57:18 +0800

    commit message

--- src/main/java/com/wbf/git/GitTest.java
+++ src/main/java/com/wbf/git/GitTest.java
@@ -2,6 +2,7 @@ package com.wbf.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello gitl");
+        System.out.println("hello git1");
+        System.out.println("hello git2");
     }
 }
